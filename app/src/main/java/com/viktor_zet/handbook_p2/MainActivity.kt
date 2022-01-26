cd523348f5f8fde24e572129798a43d8504ea2f8
Viktor <viktorzahurskiy@gmail.com>
2022-01-26 16:40:14 +0200

    ListView testing

--- app/src/main/java/com/viktor_zet/handbook_p2/MainActivity.kt
+++ app/src/main/java/com/viktor_zet/handbook_p2/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.viktor_zet.handbook_p2
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.ArrayAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val listView = findViewById<ListView>(R.id.listView)
+        val itemList = mutableListOf("First", "Second", "Third", "Fourth", "Fifth")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, itemList)
+        listView.adapter=adapter
+
     }
+
 }
